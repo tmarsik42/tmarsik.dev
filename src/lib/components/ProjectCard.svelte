d3d78d3e9bb1f62817fed862d0d33db3c60dcb62
tmarsik42 <tad.marsik@gmail.com>
2025-02-14 21:59:02 +0100

    feat: Component for projects

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { Project } from "../../customTypes";
+
+    const { id, name, description, thumbnailUrl }: Project = $props();
+</script>
+
+<article>
+    <img src={thumbnailUrl} alt="Project {name}" />
+    <h3>{name}</h3>
+    <p>
+        {description}
+        <a href="/projects/{id}">Learn More</a>
+    </p>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,44 @@
+<script lang="ts">
+    import ProjectCard from "$lib/components/ProjectCard.svelte";
+    import type { Project } from "../customTypes";
+    import type { Component } from "svelte";
+
+    const projects: Project[] = [
+        {
+            id: "1",
+            name: "tmarsik.dev",
+            description:
+                "Personal website showcasing my skills, portfolio, blog, and contact information.",
+            thumbnailUrl: "https://picsum.photos/id/237/400/600"
+        },
+        {
+            id: "2",
+            name: "VPS",
+            description: "Management of a VPS this website runs on.",
+            thumbnailUrl: "https://picsum.photos/id/237/400/600"
+        }
+    ];
+</script>
+
 <section>
     <h1>Hi, I’m Tadeáš Maršík, a Frontend Developer.</h1>
     <p>I build fast, responsive, and user-friendly web applications.</p>
-    <a href="/cv">View My CV</a>
+    <a href="/about-me">Learn more about me</a>
 </section>
 
 <section>
     <h2>Featured Projects</h2>
     <div>
-        <article>
-            <h3>Project 1</h3>
-            <p>
-                Description
-                <a href="/projects/project1">Learn More</a>
-            </p>
-        </article>
+        {#each projects as project (project.id)}
+            <ProjectCard
+                id={project.id}
+                name={project.name}
+                description={project.description}
+                thumbnailUrl={project.thumbnailUrl}
+            />
+        {:else}
+            <em>no projects yet...</em>
+        {/each}
     </div>
 </section>
 
