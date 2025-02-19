d73579905490bb1caa9bc31d7f37fcbdbd1bc2a7
tmarsik42 <tad.marsik@gmail.com>
2025-02-19 19:33:57 +0100

    feat: Regression for a quick cv

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
     import "../app.css";
-    import Header from "$lib/components/Header.svelte";
-    import Footer from "$lib/components/Footer.svelte";
 
     const { children } = $props();
 </script>
 
-<Header />
-
 <main>
     {@render children()}
 </main>
-
-<Footer />
